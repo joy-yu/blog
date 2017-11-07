32ba14d9923d1eaf8b9d627ace44e3a83202ea18
joy-yu <775154773@qq.com>
2017-11-07 23:31:52 +0800

    add gulp-zip

--- gulpfile.js
+++ gulpfile.js
@@ -3,10 +3,18 @@ var minifycss = require('gulp-clean-css');
 var uglify = require('gulp-uglify');
 var htmlmin = require('gulp-htmlmin');
 var htmlclean = require('gulp-htmlclean');
+var gzip = require('gulp-gzip');
+
+var gzip_config = {
+  threshold: '1kb',
+  gzipOptions: { level: 9 },
+  skipGrowingFiles : true
+}
 
 gulp.task('minify-css', function() {
   return gulp.src('./public/**/*.css')
   .pipe(minifycss())
+  .pipe(gzip(gzip_config))
   .pipe(gulp.dest('./public'));
 });
 
@@ -19,12 +27,14 @@ gulp.task('minify-html', function() {
     minifyCSS: true,
     minifyURLs: true,
   }))
+  .pipe(gzip(gzip_config))
   .pipe(gulp.dest('./public'))
 });
 
 gulp.task('minify-js', function() {
   return gulp.src('./public/**/*.js')
   .pipe(uglify())
+  .pipe(gzip(gzip_config))
   .pipe(gulp.dest('./public'));
 });
 
